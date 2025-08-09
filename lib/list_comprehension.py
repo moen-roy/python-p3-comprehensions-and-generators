cf178515c717098724bc49d94966ae3758aa8f0c
moen <moenr49@gmail.com>
2025-08-09 22:23:44 +0300

    list comprehensions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    evens= [x for x in num_list if x % 2 ==0]
+    return evens
     pass
 
 def make_exclamation(sentence_list):
+    exclamation= [n + "!" for n in sentence_list]
+    # sentence_list.append("!")
+    return exclamation
     pass
